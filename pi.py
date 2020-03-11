81a844da06039eb72661936832a49c0a8f95bb9e
hartleybrent <61992797+hartleybrent@users.noreply.github.com>
2020-03-11 14:35:31 -0400

    Create pi.py

---
+++ pi.py
@@ -0,0 +1,43 @@
+# For PiDay 2020 - Created by Brent Hartley using the Chudnovsky Algorithm
+# Program called Pi.py (pronounced pie (dot) pie), using a function called PiDay, and
+# Run on a raspberry pi, on 3-14-2020
+# Variables spell p, i, d, a, y, because, why not?
+# Total digits caculated? 314159, because, why not?
+# Change variables caculated by editing if statement on line 41
+import decimal
+import sys
+import string
+
+# Define the Chudnovsky Algorithm and call it PiDay because why not?
+
+def PiDay():
+    x, p, i, d, a, y,  = 1, 0, 1, 1, 3, 3, 
+    while True:
+        if 4*x+p-i < a*i:
+            yield a
+            ap = 10*(p-a*i)
+            a  = ((10*(3*x+p))//i)-10*a
+            x  *= 10
+            p  = ap
+        else:
+            ap = (2*x+p)*y
+            aa = (x*(7*d)+2+(p*y))//(i*y)
+            x  *= d
+            i  *= y
+            y  += 2
+            d += 1
+            a  = aa
+            p  = ap
+pi_digits = PiDay()
+i = 0
+z = 0
+for d in pi_digits:
+    sys.stdout.write(str(d))
+    # Add 1 to the line break counter
+    i += 1
+    # Add 1 to the termination counter
+    z += 1
+    # Define the number of digits to caculate before termination
+    if z == 314159: sys.exit("Caculation Complete")
+    # Print a line break after 60 digits and reset the line break counter
+    if i == 60: print(""); i = 0;
